08fddd91eae2e282a83ed011c70a4cb4ce5512f0
Abdul Haseeb <abdul.haseeb@contractken.com>
2024-10-01 17:26:10 +0530

    hero component and other stylings added to initial code

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import Hero from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      <div>Hello, this is page.tsx</div>
+    <main className="relative bg-black-100 flex justify-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className=" w-full">
+        <Hero />
+      </div>
     </main>
   );
 }
